deb9fdc17c8d250a824a09490a145131cee3fc02
Vesselin <tzvetkov@bg-soft.de>
2017-07-15 23:46:27 +0200

    density map in gary

--- src/mandelbrot.py
+++ src/mandelbrot.py
@@ -1,5 +1,5 @@
 #
-#   This make Mandelbrot set and save it as pictire
+#   This make Mandelbrot set and save it as picture
 #
 #         - normal way - counting the interaction to bail out to  infinity
 #         - showing the dark side - making a density map
@@ -17,17 +17,25 @@
 #
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 # consts
 picWidth,picHeight= 500, 500
 reMin, reMax = -2,1
 imMin, imMax = -1,1
 filenameInteraction = "output/mandelbrotNormal.bmp"
-fileNameDensity = "output/mandelbrotDensity.bmp"
+filenameDensity = "output/mandelbrotDensity.bmp"
 
+
+# global variable
 
 data = np.ones((picWidth,picHeight)) * 0
 dataDensity = np.ones((picWidth,picHeight)) * 0
+
+# here we store all points. One dimensional array with complex numbers
+points = []
+
+
 
 def evaluate(re,im):
 # evaluates if the point is finite
@@ -35,12 +43,26 @@ def evaluate(re,im):
     i=0
     z=complex(0,0)
     while (abs(z)<3) and (i<500):
+        points.append(z)
         z=z*z + c
         i=i+1
     return i
 
+def generateDensityMap ():
+# this generate density map
+# the bin siye of the histogram is the picture point
+    for i in range(len(points)):
+        # generate from complex number the coordinates in the pic
+        picX =int( (-reMin+points[i].real) * picWidth / (-reMin+reMax) )
+        picY =int( (-imMin+points[i].imag) * picHeight / (-imMin+imMax) )
+
+        if (picX<picWidth) and (picY<picHeight):
+            dataDensity[picX][picY] += 1
+
+
 for x in range(0, picWidth):
     for y in range(0, picHeight):
+        # generate from point in the pic them complex number
         reX=reMin + x * (-reMin + reMax ) / picWidth
         reY=imMin + y * (-imMin + imMax ) / picHeight
         color=evaluate(reX,reY)*10
@@ -53,9 +75,9 @@ im = im.convert("RGB")
 im.save(filenameInteraction)
 im.show()
 
-# density map
-# TBD
-#imDensity = Image.fromarray(dataDensity)
-#imDensity = im.convert("RGB")
-#imDensity.save(fileNameDensity)
-#imDensity.show()
+# generate and show the density map
+generateDensityMap()
+imDensity = Image.fromarray(dataDensity)
+imDensity = imDensity.convert("RGB")
+imDensity.save(filenameDensity)
+imDensity.show()
