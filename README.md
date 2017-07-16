b2afdac048f97fbbb766dae8d6f24b0c276062bb
Vesselin <tzvetkov@bg-soft.de>
2017-07-17 00:40:12 +0200

    density map and overlay

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# mandelbrot
-Generates a mandelbrot set in two ways interactions and density map 
+# Mandelbrot set
+Generates a mandelbrot set in two ways interactions, density map andoverlay
+
+         - normal way - counting the interaction to bail out to  infinity
+         - showing the dark side - making a density map
+         - making overlay of normal and density map
+
+    Edit the constants to map to your enviroments
+
+# INSTALLATION
+
+     - you need a python
+     - python modules python and PIL
+
+#   TBD: nice heat map
+
+   Autor: Vesselin Tzvetkov 2017
+   Licence: Apache 2.0
--- src/mandelbrot.py
+++ src/mandelbrot.py
@@ -3,6 +3,7 @@
 #
 #         - normal way - counting the interaction to bail out to  infinity
 #         - showing the dark side - making a density map
+#         - making overlay of normal and density map
 #
 #    Edit the constants to map to your enviroments
 #
@@ -11,42 +12,52 @@
 #     - you need a python
 #     - python modules python and PIL
 #
+#   TBD: nice heat map
 #
 #   Autor: Vesselin Tzvetkov 2017
 #   Licence: Apache 2.0
 #
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
+from PIL import ImageOps
+
 
 # consts
 picWidth,picHeight= 500, 500
 reMin, reMax = -2,1
 imMin, imMax = -1,1
 filenameInteraction = "output/mandelbrotNormal.bmp"
+
 filenameDensity = "output/mandelbrotDensity.bmp"
+filenameOverlay = "output/mandelbrotOverlay.bmp"
 
 
 # global variable
 
 data = np.ones((picWidth,picHeight)) * 0
-dataDensity = np.ones((picWidth,picHeight)) * 0
+# TBD not used for now, but nice colors are always good
+dataRGB = np.ones((picWidth,picHeight,3), 'uint8') * 0
+
+dataDensity = np.ones((picWidth,picHeight)) * 1
 
 # here we store all points. One dimensional array with complex numbers
 points = []
-
+tmp=[]
 
 
 def evaluate(re,im):
 # evaluates if the point is finite
     c = complex(re,im)
-    i=0
+    i,c1,c2,c3 =0,0,0,0
     z=complex(0,0)
-    while (abs(z)<3) and (i<500):
-        points.append(z)
+
+    while (abs(z)<3) and (i<1000):
+        if z!=complex(0,0):
+            points.append(z)
+
         z=z*z + c
         i=i+1
-    return i
+    return i,c1,c2,c3
 
 def generateDensityMap ():
 # this generate density map
@@ -57,7 +68,7 @@ def generateDensityMap ():
         picY =int( (-imMin+points[i].imag) * picHeight / (-imMin+imMax) )
 
         if (picX<picWidth) and (picY<picHeight):
-            dataDensity[picX][picY] += 1
+            dataDensity[picX][picY] *= 1.05
 
 
 for x in range(0, picWidth):
@@ -65,19 +76,26 @@ for x in range(0, picWidth):
         # generate from point in the pic them complex number
         reX=reMin + x * (-reMin + reMax ) / picWidth
         reY=imMin + y * (-imMin + imMax ) / picHeight
-        color=evaluate(reX,reY)*10
-        data [x][y]=color
-# TBD 
+        color,c1,c2,c3 =evaluate(reX,reY)
+        # TBD, to be scaled
+        data [x][y]=color*7
+
 
 # Interaction map
-im = Image.fromarray(data)
-im = im.convert("RGB")
+im = Image.fromarray(data).convert("RGB")
+im = ImageOps.invert(im)
 im.save(filenameInteraction)
 im.show()
 
+
 # generate and show the density map
 generateDensityMap()
-imDensity = Image.fromarray(dataDensity)
-imDensity = imDensity.convert("RGB")
+imDensity = Image.fromarray(dataDensity).convert("RGB")
+imDensity = ImageOps.invert(imDensity)
 imDensity.save(filenameDensity)
 imDensity.show()
+
+# make an overlay of normal and density
+imOverlay = Image.blend(im, imDensity, 0.3)
+imOverlay.show()
+imOverlay.save(filenameOverlay)
